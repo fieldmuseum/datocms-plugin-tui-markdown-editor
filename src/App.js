f3a091acd8dd8dda9f190b3b387b92cbe65096df
Roger Tuan <arcataroger@gmail.com>
2021-06-18 15:01:19 -0700

    Basic functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,104 @@
-import logo from './logo.svg';
-import './App.css';
+import SortableTree, {changeNodeAtPath, removeNodeAtPath} from 'react-sortable-tree-patch-react-17';
+import 'react-sortable-tree-patch-react-17/style.css';
+import {useState} from "react";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const availablePosts = [
+        {
+            id: 1,
+            title: "my sample post",
+            slug: "/my-sample-post",
+            children: [
+                {
+                    id: 5,
+                    title: "my child",
+                    slug: "/my-child",
+                    children: [
+                        {
+                            id: 6,
+                            title: "my grandchild",
+                            slug: "/my-grandchild"
+                        },
+                    ],
+                },
+            ],
+        },
+        {
+            id: 2,
+            title: "my sample post 2",
+            slug: "/my-sample-post-2"
+        },
+        {
+            id: 3,
+            title: "my sample post 3",
+            slug: "/my-sample-post-3"
+        },
+    ];
+
+    const [treeData, setTreeData] = useState(availablePosts);
+    const getNodeKey = ({treeIndex}) => treeIndex;
+
+    return (
+        <>
+
+            <div style={{height: 300}}>
+                <SortableTree
+                    treeData={treeData}
+                    onChange={setTreeData}
+                    generateNodeProps={({node, path}) => {
+
+                        return ({
+                            title: <input value={node.title}
+                                          onChange={event => setTreeData(
+                                              changeNodeAtPath({
+                                                  treeData,
+                                                  path,
+                                                  getNodeKey,
+                                                  newNode: {...node, title: event.target.value},
+                                              })
+                                          )}
+                            />,
+                            buttons: [
+                                <button
+                                    onClick={() => setTreeData(
+                                        changeNodeAtPath({
+                                            treeData,
+                                            path,
+                                            getNodeKey,
+                                            newNode: {...node, title: 'Booyah I changed'},
+                                        })
+                                    )}
+                                >
+                                    Rename
+                                </button>,
+                                <button
+                                    onClick={() =>
+                                        setTreeData(
+                                            removeNodeAtPath({
+                                                treeData,
+                                                path,
+                                                getNodeKey
+                                            })
+                                        )
+                                    }
+                                >
+                                    Remove
+                                </button>
+                            ]
+                        })
+                    }
+}
+/>
+
+
+</div>
+
+<pre>
+                    {JSON.stringify(treeData, null, 2)}
+                        </pre>
+</>
+)
 }
 
 export default App;
